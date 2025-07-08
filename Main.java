3944e100cc1d85a8242831367759d8ab1121f65e
itsPedro <phfaraco@gmail.com>
2025-07-08 15:42:10 -0400

    create temp Main class for testing

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+    InputHandler input = new InputHandler("./NameFile.txt", "./CourseFile.txt");
+    ArrayList<Student> studentList = new ArrayList<Student>();
+    ArrayList<Course> courseList = new ArrayList<Course>();
+    try {
+      input.readNameFile(studentList);
+    } catch (Exception e) {
+      System.out.println(e.toString());
+    }
+    for (Student val : studentList) {
+      System.out.println(val.getStudentID());
+      System.out.println(val.getStudentName());
+
+    }
+    try {
+      input.readCourseFile(courseList);
+    } catch (Exception e) {
+      System.out.println(e.toString());
+    }
+    for (Course val : courseList) {
+      System.out.println(val.getStudentID());
+      System.out.println(val.getCourseID());
+      System.out.println(val.getTest1());
+      System.out.println(val.getFinalExam());
+
+    }
+  }
+}
