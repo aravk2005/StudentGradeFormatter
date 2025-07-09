87610c0305aa0a0dc33b890f78eb827b21efec2d
Isaac Hare <92610760+Gak-Attack@users.noreply.github.com>
2025-07-08 20:11:23 -0400

    Add files via upload

---
+++ OutputWriter.java
@@ -0,0 +1,36 @@
+// Overloading happening here (Constructors)
+
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+public class OutputWriter {
+  private String outputPath
+
+  OutputWriter() {
+    this.outputPath = "";
+  }
+
+  OutputWriter(String outputPath) {
+    this.outputPath = outputPath;
+  }
+
+  void setOutputPath(String outputPath) {
+    this.outputPath = outputPath;
+  }
+
+  void writeOutputFile(ArrayList<Student> studentList, ArrayList<Course> courseList) throws Exception {
+    try {
+      FileWriter myWritter = new FileWriter(this.outputPath);
+      for (student Student: studentList){
+        for (course Course: courseList){
+          if Student.getStudentID() == Course.getStudentID(){
+            myWritter.write(Student.getStudentID() + "," + Student.getStudentName() + "," + Course.getCourseID() + "," + Course.calculateFinalGrade())
+          }
+        }
+      }
+      myWritter.close();
+    } catch (Exception e) {
+      throw e;
+    }
+  }
+}
