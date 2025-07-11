b8be5571cd774fa3b28c12fecefeb6ee4c5f5c96
Isaac Hare <92610760+Gak-Attack@users.noreply.github.com>
2025-07-11 13:37:24 -0400

    Update GradeCalculator.java
    Changed it so a single student is passed into the calculator. The output is the grades of their courses.

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 
 public class GradeCalculator{
 
-    static ArrayList<Double> computeFinalGrades(ArrayList<Course> courses){
-        ArrayList<Double> finalGrades = new ArrayList<Double>();
-        for (Course co : courses){
+    static ArrayList<Double> computeFinalGrades(Student st){
+        ArrayList<Double> finalGrades = new ArrayList<>();
+        for (Course co : st.getCourses()){
             finalGrades.add(co.calculateFinalGrade());
         }
         return finalGrades;
