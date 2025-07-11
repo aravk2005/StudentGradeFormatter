ef3180ec15b975b7072a4b91045151ee7ffe1d72
Isaac Hare <92610760+Gak-Attack@users.noreply.github.com>
2025-07-11 14:48:18 -0400

    Update OutputWriter.java

--- OutputWriter.java
+++ OutputWriter.java
@@ -19,13 +19,12 @@ public class OutputWriter {
   }
 
   void writeOutputFile(ArrayList<Student> studentList, ArrayList<Course> courseList) throws Exception {
-    ArrayList<Double> stGrades; //Sets up the list of grades students have in their courses
+    ArrayList<Double> stGrades = GradeCalculator.computeFinalGrades(studentList); //Sets up the list of grades students have in their courses
     int i;//List index for the grades
     try {
       FileWriter myWriter = new FileWriter(this.outputPath);
       for (Student st: studentList){
         i = 0; //Sets the inital index to 0 for all students
-        stGrades = GradeCalculator.computeFinalGrades(st); //runs computeFinalGrades to initalize the student's grades
         for (Course co: st.getCourses){
           myWriter.write(st.getStudentID() + ", " + st.getStudentName() + ", " + co.getCourseID() + ", " +String.format( "%.1f",stGrades.get(i)) +"\n");
           i++; //increases the index to access the correct grades for the course
